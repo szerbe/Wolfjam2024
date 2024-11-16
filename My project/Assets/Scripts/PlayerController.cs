6b12c10264ddd0d13e74149517cddb56089de457
szerbe <shalinzerbe@gmail.com>
2024-11-16 00:00:39 -0500

    Update PlayerController.cs

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     void Update(){
         float horizontal = 0.0f;
         float vertical = 0.0f;
+        char rotate = 'N';
         if(Input.GetKeyDown(KeyCode.LeftArrow)){
             horizontal = -1.0f;
         }
@@ -20,9 +22,25 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.DownArrow)){
             vertical = -1.0f;
         }
+        if(Input.GetKeyDown(KeyCode.Q)){
+            rotate = 'Q';
+        }
+        if(Input.GetKeyDown(KeyCode.E)){
+            rotate = 'E';
+        }
         Vector2 position = transform.position;
         position.x = position.x + horizontal;
         position.y = position.y + vertical;
         transform.position = position;
+
+
+        void rotateDir(char rotate){
+            if(rotate == 'N'){
+                return;
+            }
+            if(rotate == 'c'){
+                
+            }
+        }
     }
 }
