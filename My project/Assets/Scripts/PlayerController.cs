6bafa17832e85e29e6c22e0ca6484e6739dd1950
szerbe <shalinzerbe@gmail.com>
2024-11-15 19:42:04 -0500

    Update PlayerController.cs

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+    public InputAction RightAction;
     void Start(){
+        LeftAction.Enable();
+        RightAction.Enable();
     }
 
     void Update(){
+        float horizontal = 0.0f;
+        if(LeftAction.IsPressed()){
+            horizontal = -1.0f;
+        }
+        if(RightAction.IsPressed()){
+            horizontal = 1.0f;
+        }
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
 }
