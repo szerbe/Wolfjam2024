fac631d452d76958fa8a01882091ec32885f8ed3
dinoanna <tejanna15@gmail.com>
2024-11-15 23:13:22 -0500

    getScene

--- My project/Assets/Scripts/Setup.cs
+++ My project/Assets/Scripts/Setup.cs
@@ -47,4 +47,8 @@ public class Block : MonoBehaviour
         }
         return false;
     }
+
+    public char[][] getScene(){
+        return scene;
+    }
 }
--- My project/Assets/Scripts/level1Init.cs
+++ My project/Assets/Scripts/level1Init.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class level1Init
 {
     public level1Init(){
-        //char[][] layout = [[]];
-        //int[][2] key = [[]];
-        int[1][2] character = [0,0];
-        //Setup lvl1 = new Setup(layout, key, character);
+        //Setup lvl1 = new Setup();
         //input 2d char array
     }
 }
