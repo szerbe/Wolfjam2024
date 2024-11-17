0435b498fed3e60357895d041659f28fff27ae5c
dinoanna <tejanna15@gmail.com>
2024-11-16 20:28:09 -0500

    change levels

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -51,8 +51,24 @@ public class PlayerController : MonoBehaviour
         }
 
         //Confirm choices (level select, attachment, removal)
-        if(Input.GetKeyDown(KeyCode.Return)){
+        if(Input.GetKeyDown(KeyCode.Alpha1)||Input.GetKeyDown(KeyCode.Keypad1)){
             levelInit one = new levelInit(1);
+            // confirm = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)||Input.GetKeyDown(KeyCode.Keypad2)){
+            levelInit one = new levelInit(2);
+            // confirm = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)||Input.GetKeyDown(KeyCode.Keypad3)){
+            levelInit one = new levelInit(3);
+            // confirm = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)||Input.GetKeyDown(KeyCode.Keypad4)){
+            levelInit one = new levelInit(4);
+            // confirm = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5)||Input.GetKeyDown(KeyCode.Keypad5)){
+            levelInit one = new levelInit(5);
             // confirm = true;
         }
         //Spawn more tiles (debug)
